d028dd3de1a59fb126245f142f7a6fc571cad1a9
fauzanazhari <fauzan.azharrahman04@gmail.com>
2024-08-14 01:40:25 +0800

    selesai

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import '@/app/ui/global.css';
+// import '@/app/ui/acme-logo'
 import Link from 'next/link';
 
 export default function Page() {
   return (
-    <main className="flex flex-col min-h-screen p-6">
-      <div className="flex items-end h-20 p-4 bg-blue-500 rounded-lg shrink-0 md:h-52">
+    <main className="flex min-h-screen flex-col p-6">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
         <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
       </div>
@@ -25,7 +26,18 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          {/* Gambar desktop */}
+          <img
+            src="/hero-desktop.png"
+            alt="Screenshots of the dashboard project showing desktop version"
+            className="hidden md:block w-full h-auto rounded-md"
+          />
+          {/* Gambar mobile */}
+          <img
+            src="/hero-mobile.png"
+            alt="Screenshots of the dashboard project showing mobile version"
+            className="block md:hidden w-[560px] h-[620px] rounded-md"
+          />
         </div>
       </div>
     </main>
