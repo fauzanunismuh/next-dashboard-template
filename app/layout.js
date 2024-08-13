7d49d2717184af641088bd95534ff5260cca6a21
fauzanazhari <fauzan.azharrahman04@gmail.com>
2024-08-14 02:54:06 +0800

    selesai chapter 6

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import '@/app/ui/global.css';
 import { lusitana } from './ui/fonts';
 
+// /app/layout.jsx
+import "@/app/ui/global.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
